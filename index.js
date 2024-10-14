565e2a77514b95b371f15e4a111f30b19f561b97
Rolly Maulana Awangga <awangga@gmail.com>
2024-10-14 19:48:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ getJSON("https://asia-southeast2-awangga.cloudfunctions.net/jualin/data/user","l
 function responseFunction(result){
     if (result.status === 200){
         setInner("content","Selamat datang "+result.data.name);
-        redirect("/jboard");
+        redirect("/gboard");
     }else{
         getJSON("https://asia-southeast2-awangga.cloudfunctions.net/jualin/data/konsumen/user","login",getCookie("login"),apiResponse)
     }
